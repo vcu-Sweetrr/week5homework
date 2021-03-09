fbe64b17c4c6e43a70dc7951c1dab474ea5da7a4
vcu-Sweetrr <sweetrr@vcu.edu>
2021-03-09 00:06:29 -0500

    fixed tests

--- fun/homework.py
+++ fun/homework.py
@@ -1,21 +1,10 @@
 """Homework file for my students to have fun with some algorithms! """
 
-
 def find_greatest_number(incoming_list):
     print(max(incoming_list))
-    pass
-
-
 def find_least_number(incoming_list):
     print(min(incoming_list))
-    pass
-
-
 def add_list_numbers(incoming_list):
     print(sum(incoming_list))
-    pass
-
-
 def longest_value_key(incoming_dict):
     print(max(len(incoming_dict)))
-    pass
