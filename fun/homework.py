bd6a8d8a1f75ab104d106ada687a99c7c0cad703
vcu-Sweetrr <sweetrr@vcu.edu>
2021-03-09 00:19:44 -0500

    test4

--- fun/homework.py
+++ fun/homework.py
@@ -4,14 +4,11 @@
 def find_greatest_number(incoming_list):
     print(max(incoming_list))
 
-
 def find_least_number(incoming_list):
     print(min(incoming_list))
 
-
 def add_list_numbers(incoming_list):
     print(sum(incoming_list))
-
 
 def longest_value_key(incoming_dict):
     print(max(len(incoming_dict)))
