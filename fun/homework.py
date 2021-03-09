e00d2d3969e032038f9352a6eaa46afd506f46f8
vcu-Sweetrr <sweetrr@vcu.edu>
2021-03-09 00:10:29 -0500

    added spaces

--- fun/homework.py
+++ fun/homework.py
@@ -1,10 +1,17 @@
 """Homework file for my students to have fun with some algorithms! """
 
+
 def find_greatest_number(incoming_list):
     print(max(incoming_list))
+
+
 def find_least_number(incoming_list):
     print(min(incoming_list))
+
+
 def add_list_numbers(incoming_list):
     print(sum(incoming_list))
+
+
 def longest_value_key(incoming_dict):
     print(max(len(incoming_dict)))
