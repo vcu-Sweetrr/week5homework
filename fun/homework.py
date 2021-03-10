362e780699d0139b67993dd9206571f431640e6d
vcu-Sweetrr <sweetrr@vcu.edu>
2021-03-09 19:42:43 -0500

    Fixes from in class (test)

--- fun/homework.py
+++ fun/homework.py
@@ -2,13 +2,36 @@
 
 
 def find_greatest_number(incoming_list):
-    print(max(incoming_list))
+    retval = max(incoming_list)
+    return retval
+
 
 def find_least_number(incoming_list):
-    print(min(incoming_list))
+    retval = min(incoming_list)
+    return retval
+
 
 def add_list_numbers(incoming_list):
-    print(sum(incoming_list))
+    if incoming_list:
+        retval = sum(incoming_list)
+    else:
+        retval = 0
+    return retval
+
 
 def longest_value_key(incoming_dict):
-    print(max(len(incoming_dict, key=len)))
+    if not incoming_dict:
+        return None
+
+    all_keys = incoming_dict.keys()
+    if not all_keys:
+        return None
+
+    longest_key = None
+    for key in all_keys:
+        if not longest_key:
+            longest_key = key
+
+        if len(incoming_dict[key]) > len(incoming_dict[longest_key]):
+            longest_key = key
+    return longest_key
